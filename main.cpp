950f54a1af32d418d1797e2e9afdd53a267a0a07
mleykhner <mleykhner@ya.ru>
2022-11-26 18:39:38 +0300

    Сделал кнопочки и перетаскивалку

--- main.cpp
+++ main.cpp
@@ -16,18 +16,60 @@ struct Node {
     }
 };
 
+class Button {
+public:
+    Button(Texture * texture, float height) {
+        float scale = height / texture->getSize().y;
+        button.setSize(Vector2f(texture->getSize().x * scale, texture->getSize().y * scale));
+        button.setTexture(texture);
+    }
+
+    void setPosition(sf::Vector2f point) {
+        button.setPosition(point);
+    }
+
+    void drawTo(sf::RenderWindow &window) {
+        window.draw(button);
+    }
+
+    Vector2f getSize(){
+        return button.getSize();
+    }
+
+    // Check if the mouse is within the bounds of the button:
+    bool isMouseOver(sf::RenderWindow &window) {
+        int mouseX = sf::Mouse::getPosition(window).x;
+        int mouseY = sf::Mouse::getPosition(window).y;
+
+        int btnPosX = button.getPosition().x;
+        int btnPosY = button.getPosition().y;
+
+        int btnxPosWidth = button.getPosition().x + button.getSize().x;
+        int btnyPosHeight = button.getPosition().y + button.getSize().y;
+
+        if (mouseX < btnxPosWidth && mouseX > btnPosX && mouseY < btnyPosHeight && mouseY > btnPosY) {
+            return true;
+        }
+        return false;
+    }
+private:
+    sf::RectangleShape button;
+};
+
 Texture createBackgroundTexture(Uint16 windowWidth, Uint16 windowHeight, Color color);
 void renderNodes(list <Drawable *> * renderQueue, Node * node, Font *font, int halfWidth, int verticalDistribution, int layer = 1, int xOffset = 0, int yOffset = 0);
-void clearRenderQueue(list <Drawable *> renderQueue);
+void clearRenderQueue(list <Drawable *> * renderQueue);
 
 int main() {
     //Настройки окна
     Uint16 windowWidth = 1440;
     Uint16 windowHeight = 960;
     string windowTitle = "Граф-вонючка";
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 16;
 
     //Инициализация окна
-    RenderWindow myWindow(VideoMode(windowWidth, windowHeight), String::fromUtf8(windowTitle.begin(), windowTitle.end()), Style::Close);
+    RenderWindow myWindow(VideoMode(windowWidth, windowHeight), String::fromUtf8(windowTitle.begin(), windowTitle.end()), Style::Close, settings);
     myWindow.setFramerateLimit(60);
 
     //Что-то делаем...
@@ -35,6 +77,12 @@ int main() {
     RectangleShape background;
     background.setSize(Vector2f(1440, 960));
     background.setTexture(&backgroundTexture);
+
+    Texture addNewButton_texture;
+    addNewButton_texture.loadFromFile("/Users/mleykhner/Documents/TreesDrawer/resources/addNewNode.png");
+    Texture deleteButton_texture;
+    deleteButton_texture.loadFromFile("/Users/mleykhner/Documents/TreesDrawer/resources/deleteNode.png");
+
 
     Node root(12);
     root.left = new Node(11);
@@ -48,24 +96,72 @@ int main() {
         throw "FONT_NOT_LOADED";
     }
 
+    int xOffset = 220;
+    int yOffset = 200;
+
     list <Drawable *> renderQueue;
-    renderNodes(&renderQueue, &root, &montserratBold, 500, 200, 1, 220, 200);
+    renderNodes(&renderQueue, &root, &montserratBold, 500, 200, 1, xOffset, yOffset);
+    Button addNewButton(&addNewButton_texture, 60);
+    addNewButton.setPosition(Vector2f(20, 960 - addNewButton.getSize().y - 20));
+    Button deleteButton(&deleteButton_texture, 60);
+    deleteButton.setPosition(Vector2f(40 + addNewButton.getSize().x, 960 - addNewButton.getSize().y - 20));
 
     //Начинаем жизнь окна
+    bool dragging = false;
+    int mousePositionX = 0;
+    int mousePositionY = 0;
+
     while (myWindow.isOpen()) //Пока окно открыто
     {
         sf::Event event; //Отслеживаем события
         while (myWindow.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                //При закрытии окна завершаем работу программы
-                myWindow.close();
+            switch (event.type){
+                case sf::Event::Closed:
+                    //При закрытии окна завершаем работу программы
+                    myWindow.close();
+                    break;
+                case sf::Event::MouseButtonPressed:
+                    if (addNewButton.isMouseOver(myWindow)) {
+                        std::cout << "addNew" << "\n";
+                    }
+                    else if(deleteButton.isMouseOver(myWindow)) {
+                        std::cout << "delete" << "\n";
+                    }
+                    else {
+                        dragging = true;
+                        mousePositionX = Mouse::getPosition(myWindow).x;
+                        mousePositionY = Mouse::getPosition(myWindow).y;
+                    }
+                    break;
+                case sf::Event::MouseButtonReleased:
+                    dragging = false;
+                    break;
+            }
+
         }
+
+        if (dragging){
+            int deltaX = Mouse::getPosition(myWindow).x - mousePositionX;
+            int deltaY = Mouse::getPosition(myWindow).y - mousePositionY;
+
+            clearRenderQueue(&renderQueue);
+            xOffset+=deltaX;
+            yOffset+=deltaY;
+
+            renderNodes(&renderQueue, &root, &montserratBold, 500, 200, 1, xOffset, yOffset);
+
+            mousePositionX = Mouse::getPosition(myWindow).x;
+            mousePositionY = Mouse::getPosition(myWindow).y;
+        }
+
         myWindow.clear();
         myWindow.draw(background);
         for(Drawable * toDraw : renderQueue){
             myWindow.draw(* toDraw);
         }
+        addNewButton.drawTo(myWindow);
+        deleteButton.drawTo(myWindow);
         myWindow.display();
     }
 
@@ -139,12 +235,9 @@ void renderNodes(list <Drawable *> * renderQueue, Node * node, Font *font, int h
         renderNodes(renderQueue, node->right, font, halfWidth + halfWidth / pow(2, layer),  verticalDistribution, layer + 1, xOffset, yOffset);
 }
 
-void clearRenderQueue(list <Drawable *> renderQueue){
-    for(Drawable * toRender : renderQueue){
-        delete toRender;
+void clearRenderQueue(list <Drawable *> * renderQueue){
+    while(!renderQueue->empty()){
+        delete *renderQueue->begin();
+        renderQueue->pop_front();
     }
-}
-
-void drawButtons(list <Drawable *> * renderQueue){
-
 }
